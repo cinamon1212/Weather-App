5421ccdd3b274ac64d8b99f0b73813e7bd25a0cd
Andrey Kuzmin <andrey.kuzmin.2806@mail.ru>
2024-08-12 15:06:06 +0300

    delete react memo in WeatherParams and WeatherImageByStatus

--- src/entities/WeatherImageByStatus/ui/WeatherImageByStatus.tsx
+++ src/entities/WeatherImageByStatus/ui/WeatherImageByStatus.tsx
@@ -1,16 +1,15 @@
 import { WeatherStatus, useAppTheme } from '@/shared'
 import { Image } from '@chakra-ui/react'
 import { getWeatherPathFromStatus } from '../helpers'
-import { memo } from 'react'
 import { weatherImgStyles } from './styles'
 
 type Props = {
   status: WeatherStatus
 }
 
-export const WeatherImageByStatus = memo(({ status }: Props) => {
+export const WeatherImageByStatus = ({ status }: Props) => {
   const imgSrc = getWeatherPathFromStatus(status)
   const { animations } = useAppTheme()
 
   return <Image {...weatherImgStyles} src={imgSrc} animation={animations.imgAnimation} />
-})
+}
--- src/entities/WeatherParams/ui/WeatherParams.tsx
+++ src/entities/WeatherParams/ui/WeatherParams.tsx
@@ -1,6 +1,5 @@
 import { defineStyle, Stack } from '@chakra-ui/react'
 import { WeatherParam } from './WeatherParam'
-import { memo } from 'react'
 
 const stackStyles = defineStyle({
   justifyContent: 'space-between',
@@ -17,7 +16,7 @@ type Props = {
   wind: number
 }
 
-export const WeatherParams = memo(({ humidity, wind }: Props) => {
+export const WeatherParams = ({ humidity, wind }: Props) => {
   const fields = [
     {
       iconName: 'humidity',
@@ -32,6 +31,7 @@ export const WeatherParams = memo(({ humidity, wind }: Props) => {
       justifyContent: { base: 'center', sm: 'flex-end' },
     },
   ]
+
   return (
     <Stack {...stackStyles}>
       {fields.map((field) => (
@@ -39,4 +39,4 @@ export const WeatherParams = memo(({ humidity, wind }: Props) => {
       ))}
     </Stack>
   )
-})
+}
